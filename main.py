2c4fb38132aade2a21e409c8ce30ceabe62f0a42
jirizikes <zikes@collabim.com>
2019-03-01 01:49:44 +0100

    Generic writer to SQS

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ csvlt = '\n'
 csvdel = ','
 csvquo = '"'
 
-with open('/data/in/tables/source.csv', mode='rt', encoding='utf-8') as in_file:
+with open(parameters['INPUT'], mode='rt', encoding='utf-8') as in_file:
     lazy_lines = (line.replace('\0', '') for line in in_file)
     reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel, quotechar=csvquo)
     for row in reader:
